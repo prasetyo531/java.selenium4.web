3e4d33cc76d899a7bffe8e9b459c777460398687
prasetyo <prasetyo531@gmail.com>
2025-03-22 11:23:12 +0700

    delete file const

--- src/main/java/constants/constants.java
+++
@@ -1,7 +0,0 @@
-package constants;
-
-public class constants {
-
-    public final static String CHROME_DRIVER_DIRECTORY = System.getProperty("user.dir") + "//chromedriver";
-    public final static String GECKO_DRIVER_DIRECTORY = System.getProperty("user.dir") + "//geckodriver";
-}
