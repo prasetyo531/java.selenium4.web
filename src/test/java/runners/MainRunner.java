38f5a65715a68ba2eee748e8a35e9c395b6aea94
prasetyo <prasetyo531@gmail.com>
2025-05-05 15:49:46 +0700

    adjust to single mainrunner to track all test in cucumber-report.html

--- src/test/java/runners/MainRunner.java
+++ src/test/java/runners/MainRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
-        tags = "@Scenario3", monochrome = true, dryRun = false,
+        tags = "@Regression", monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber", "html:target/cucumber-reports.html", "json:target/cucumber.json"})
-public class ThreeRunner extends AbstractTestNGCucumberTests {
+public class MainRunner extends AbstractTestNGCucumberTests {
 }
